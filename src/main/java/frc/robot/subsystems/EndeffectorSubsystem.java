233f9fc7ddc0ca50a682bdb6fb9d466ce6cac34a
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2025-10-26 20:55:30 -0400

    rename files + advantagescope assets

--- src/main/java/frc/robot/subsystems/EndeffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/EndeffectorSubsystem.java
@@ -5,15 +5,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Ports;
 
-public class EndeffectorSubsystem extends SubsystemBase {
+public class EndEffectorSubsystem extends SubsystemBase {
 
     private TalonFX IntakeMotor = new TalonFX(Ports.Endeffector.IntakeMotor);
     private TalonFX CenteringMotor = new TalonFX(Ports.Endeffector.CenteringMotor);
 
-    public EndeffectorSubsystem() {
-        
+    public EndEffectorSubsystem() {
+
     }
-
-
 
 }
